bcbef80b5264a7c750b60e1dfc246eb366148ca5
Grace BK <iris.b1991@gmail.com>
2017-11-03 16:51:26 +0100

    Initial commit
    Creation du repertoir initial du projet

---
+++ projet_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  projet_cpp
+//
+//  Created by Grace on 03/11/2017.
+//  Copyright © 2017 Grace BOUKOU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
