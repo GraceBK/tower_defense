0cc843a85ca2522bf6b75246701a2aee12a75866
Grace BK <iris.b1991@gmail.com>
2017-11-03 17:32:55 +0100

    Import des fichiers de Base au projet

--- projet_cpp/src/main.cpp
+++ projet_cpp/src/main.cpp
@@ -8,8 +8,26 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+#include "Engine.h"
+#include "MyControlEngine.h"
+#include "MyGameEngine.h"
+#include "MyGraphicEngine.h"
+
+int main(int argc, char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    Engine e(argc, argv);
+    std::vector<Papillon *> paps;
+    
+    GraphicEngine *ge = new MyGraphicEngine(&paps);
+    GameEngine *gme = new MyGameEngine(&paps);
+    ControlEngine *ce = new MyControlEngine(&paps);
+    
+    e.setGraphicEngine(ge);
+    e.setGameEngine(gme);
+    e.setControlEngine(ce);
+    
+    e.start();
     return 0;
 }
